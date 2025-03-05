40113a625f194211ad445adf6238bdde88bfc0ac
LinnikA <a@linnik-1.ru>
2025-03-05 13:35:53 +0300

    added user and controller

---
+++ AuthenticationService/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationService.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    public UserController()
+    {
+        var logger = new Logger();
+        logger.WriteEvent("сообщение о событии в программе");
+        logger.WriteError("сообщение об ошибке в программе");
+    }
+    [HttpGet]
+    public User GetUser()
+    {
+        return new User
+        {
+            Id = new Guid(),
+            FirstName = "Anton",
+            LastName = "Antonov",
+            Login = "anton",
+            Password = "123",
+            Email = "ant@mail.com"
+        };
+    }
+
+
+
+
+}
+
---
+++ AuthenticationService/Logger.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthenticationService
+{
+    public class Logger
+    {
+        public void WriteEvent(string eventMessage)
+        {
+            Console.WriteLine(eventMessage);
+        }
+        public void WriteError(string errorMessage)
+        {
+            Console.WriteLine(errorMessage);
+        }
+    }
+}
